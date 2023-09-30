f3d3416b64637f7d072858aea9f4dca019006bfa
Corey Farwell <coreyf@rwell.org>
2023-09-29 21:23:00 -0400

    init commit

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,9 @@
-FROM centos:7
-MAINTAINER Denis Kolodin <deniskolodin@gmail.com>
+FROM rust:1.72.1
 
 EXPOSE 8080
 
-ENV SOURCES=/sources
+WORKDIR /opt/myapp
+COPY . .
 
-RUN yum update -y
-RUN yum install -y file gcc openssl-devel
-RUN curl -sSf https://static.rust-lang.org/rustup.sh | sh -s -- --disable-sudo
-
-RUN mkdir -p $SOURCES
-ADD ./ $SOURCES
-
-WORKDIR $SOURCES
 RUN cargo build --release
-
 CMD cargo run --release
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,58 @@
-#[macro_use]
-extern crate rustful;
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-#[macro_use]
-extern crate log;
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
 extern crate env_logger;
+extern crate log;
 
-use std::error::Error;
-
-use rustful::{Server, Context, Response, TreeRouter};
-
-fn response_ok(_: Context, response: Response) {
-    response.send("OK!");
+async fn hello(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
 
-fn main() {
-    env_logger::init().unwrap();
-    let server_result = Server {
-        host: 8080.into(),
-        handlers: insert_routes!{
-            TreeRouter::new() => {
-                Get: response_ok,
-                "_ah" => {
-                    Get: response_ok,
-                    "start" => Get: response_ok,
-                    "stop" => Get: response_ok,
-                    "health" => Get: response_ok
-                }
-            }
-        },
-        ..Server::default()
-    }.run();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
-    match server_result {
-        Ok(_server) => {},
-        Err(e) => error!("could not start server: {}", e.description()),
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            println!("HI");
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
     }
+}
+
+fn build_url(path: &str) -> String {
+    format!("https://inaturalist.org/api/#{path}")
+}
+
+fn build_request(method: &str, path: &str) -> Result<reqwest::Request, reqwest::Error> {
+    // TODO: don't hardcode the method constant below
+    reqwest::Client::new()
+        .request(reqwest::Method::GET, build_url(path))
+        .build()
 }
